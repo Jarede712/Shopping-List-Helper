7607369d0d057f1b1f7d8657ad59945a607458c0
Jarede712 <jaredeic@yahoo.com>
2024-01-22 20:15:54 -0800

    added functionality to all controllers, added controllers for all of the existing models

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
+require("dotenv").config();
 const express = require("express");
 const exphbs = require("express-handlebars");
 const sequelize = require("./utils/connection"); // Updated path to database connection
+const session = require("express-session");
 const PORT = process.env.PORT || 3000;
 
 const app = express();
@@ -8,7 +10,17 @@ const app = express();
 // Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static("public"));
+app.use(express.static("public")); // make sure this directory exists
+
+// Session middleware
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET, // make sure this variable is in the .env file
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: "auto" },
+  })
+);
 
 // Handlebars middleware
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
@@ -18,8 +30,8 @@ app.set("view engine", "handlebars");
 const apiRoutes = require("./routes/apiRoutes");
 const htmlRoutes = require("./routes/htmlRoutes");
 
-app.use("/api", apiRoutes); // Use '/api' as the base URL for API routes
-app.use("/", htmlRoutes); // Use '/' as the base URL for HTML routes
+app.use("/api", apiRoutes); // '/api' is the base URL for API routes
+app.use("/", htmlRoutes); // '/' is the base URL for HTML routes
 
 // Start the server
 sequelize.sync({ force: false }).then(() => {
