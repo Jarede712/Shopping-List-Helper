1dcc387b169262a8dc4d0d6b5b6f0a9bca47735d
Jarede712 <jaredeic@yahoo.com>
2024-01-25 22:15:21 -0800

    fixed bug with categories not appearing in a GET request

--- models/index.js
+++ models/index.js
@@ -1,44 +1,39 @@
 // import models
-const Inventory = require('./Inventories');
-const Category = require('./Categories');
-const List = require('./List');
-const User = require('./User');
-const Userlist = require('./Userlist');
-
+const Inventory = require("./Inventories");
+const Category = require("./Categories");
+const List = require("./List");
+const User = require("./User");
+const Userlist = require("./Userlist");
 
 Inventory.belongsTo(Category, {
-  foreignKey: 'category_id'
+  foreignKey: "category_id",
 });
-
 
 Category.hasMany(Inventory, {
-  foreignKey: 'category_id',
-  onDelete: 'SET NULL'
-})
-
-
-List.belongsTo(User, {
-  foreignKey: 'user_id'
+  foreignKey: "category_id",
+  onDelete: "SET NULL",
 });
 
+List.belongsTo(User, {
+  foreignKey: "user_id",
+});
 
 User.hasMany(List, {
-  foreignKey: 'user_id',
-  onDelete: 'SET NULL'
-})
+  foreignKey: "user_id",
+  onDelete: "SET NULL",
+});
 
 // Products belongToMany Tags (through ProductTag)
 List.belongsToMany(Inventory, {
   through: Userlist,
-  foreignKey: 'list_id'
-})
+  foreignKey: "list_id",
+});
 
 // Tags belongToMany Products (through ProductTag)
 Inventory.belongsToMany(List, {
   through: Userlist,
-  foreignKey: 'inventory_id'
-})
-
+  foreignKey: "inventory_id",
+});
 
 module.exports = {
   Inventory,
