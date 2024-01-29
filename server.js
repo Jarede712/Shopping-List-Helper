fb22fd5e16af4b44d5b83774bb4b7160fb9e798a
ulisesd404 <144632800+ulisesd404@users.noreply.github.com>
2024-01-28 17:10:11 -0800

    connected the login and logout

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const exphbs = require("express-handlebars");
 const sequelize = require("./config/connection"); // Updated path to database connection
 const session = require("express-session");
 const nodemailer = require('nodemailer');
+const jwt = require('jsonwebtoken');
 const PORT = process.env.PORT || 3001;
 
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
